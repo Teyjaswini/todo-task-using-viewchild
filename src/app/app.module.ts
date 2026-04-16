4a0f3f52eb9f3dcdda7732393441fe3292031d0b
Teyjaswini Wakhare <wakhareteju@gmail.com>
2026-04-16 20:00:07 +0530

    work on todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './shared/components/todos/todos.component';
-import { StudentTaskComponent } from './shared/components/student-task/student-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    StudentTaskComponent
   ],
   imports: [
     BrowserModule
